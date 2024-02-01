9453a12adebedd3af4ed14937fb412efe4a4a3b9
David Donachie <david.donachie@snp.org>
2024-02-01 10:03:54 +0000

    Update include-part to v2

--- include-part-macro/README.md
+++ include-part-macro/README.md
@@ -2,6 +2,9 @@
 
 The `<<include-part>>` macro is a simple extension of SugarCube's built-in `<<include>>` which includes part of another passage instead of the whole thing.
 
+> [!WARNING]
+> Version 1 of this macro incorrectly ran SugarCube code present in the parts of the passage you were *not* including. Version 2 does not do this. If you are using version 1, you should upgrade.
+
 ### Installation ###
 
 Add the contents of [include-part.js](include-part.js) to your story Javascript.
--- include-part-macro/include-part.js
+++ include-part-macro/include-part.js
@@ -1,7 +1,25 @@
 Macro.add("include-part", {
-    skipArgs: false,
-    handler : function() {
-        const passage = $("<div>").wiki(Story.get(this.args[0]).processText());
-      jQuery(this.output).append(passage.find(this.args[1]));
+  skipArgs: false,
+  handler : function() {
+    if (Story.has(this.args[0])) {
+      // get the source and escape it
+      let   source  = Story.get(this.args[0]).processText();
+            source  = source.replaceAll('<<','{{%');
+            source  = source.replaceAll('>>','%}}');
+    
+      // find the element required
+      const passage = $("<div>").append(source);
+      let   element = passage.find(this.args[1]);
+      if (this.args[2]) { element = element.children(); }
+
+      if (element.length) {
+        // convert source back to SC
+              source  = element.prop('outerHTML');
+              source  = source.replaceAll('{{%','<<');
+              source  = source.replaceAll('%}}','>>');
+
+        jQuery(this.output).wiki(source);
+      }
     }
-  });
+  }
+});
