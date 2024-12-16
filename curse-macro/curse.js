963247d2ec5891475904ac0ec0d502b53b8663be
David Donachie <stranger@teuton.org>
2024-12-16 22:32:35 +0000

    Allow cursedness argument

--- curse-macro/curse.js
+++ curse-macro/curse.js
@@ -5,17 +5,18 @@ Macro.add('curse',{
 
         const below = ['&#790;','&#791;','&#792;','&#793;','&#796;','&#797;','&#798;','&#799;','&#800;','&#801;','&#802;','&#803;','&#804;','&#805;','&#806;','&#807;','&#808;','&#809;','&#810;','&#811;','&#812;','&#813;','&#814;','&#815;','&#816;','&#817;','&#818;','&#819;','&#825;','&#826;','&#827;','&#828;','&#837;','&#839;','&#840;','&#841;','&#845;','&#846;','&#851;','&#852;','&#853;','&#854;','&#857;','&#858;','&#860;','&#863;','&#866;'];
 
-        const over  = ['&#820;','&#821;','&#822;','&#823;','&#824;'];
+        const over = ['&#820;','&#821;','&#822;','&#823;','&#824;'];
 
         const latin = ['&#867;','&#868;','&#869;','&#870;','&#871;','&#872;','&#873;','&#874;','&#875;','&#876;','&#877;','&#878;','&#879;'];
         
-        let input   = this.payload[0].contents;
+        let c        = this.args[0] ?? 3;
+        let input  = this.payload[0].contents;
         let out     = '';
         for (let i = 0; i < input.length; i++) {
-            const l_above = random(0,3);
-            const l_below = random(0,3);
-            const l_over  = random(0,1);
-            const l_latin = random(0,1);
+            const l_above = Math.floor(Math.pow(Math.random(),2) * c);
+            const l_below = Math.floor(Math.pow(Math.random(),2) * c);
+            const l_over  = random(1,10);
+            const l_latin = random(1,10);
             out += input[i];
             if (l_above) {
                 out += above.randomMany(l_above).join('');
@@ -23,13 +24,13 @@ Macro.add('curse',{
             if (l_below) {
                 out += below.randomMany(l_below).join('');
             }
-            if (l_over) {
-                out += over.randomMany(l_over).join('');
+            if (l_over <= c) {
+                out += over.randomMany(Math.ceil(c/3)).join('');
             }
-            if (l_latin) {
-                out += latin.randomMany(l_latin).join('');
+            if (l_latin <= c) {
+                out += latin.randomMany(Math.ceil(c/3)).join('');
             }
         }
-        $(this.output).html(out);
+        $(this.output).wiki(out);
     }
 });
